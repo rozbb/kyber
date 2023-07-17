7f7a97cb419c2cd98de11599e0ece7db1b7474bd
Michael Rosenberg <micro@fastmail.com>
2023-07-16 23:08:53 -0400

    Switch cmov() to use subtle's conditional_assign for swapping bytestrings

--- src/reference/verify.rs
+++ src/reference/verify.rs
@@ -1,21 +1,4 @@
-// Name:        verify
-//
-// Description: Compare two arrays for equality in constant time.
-//
-// Arguments:   const [u8] a: first byte array
-//              const [u8] b: second byte array
-//              size_t len:             length of the byte arrays
-//
-// Returns 0 if the byte arrays are equal, 1 otherwise
-pub fn verify(a: &[u8], b: &[u8], len: usize) -> u8
-{
-  let mut r = 0u64;
-  for i in 0..len {
-    r |= (a[i] ^ b[i]) as u64;
-  }
-  r = r.wrapping_neg() >> 63;
-  r as u8
-}
+use subtle::{ConditionallySelectable, Choice};
 
 // Name:        cmov
 //
@@ -28,10 +11,9 @@ pub fn verify(a: &[u8], b: &[u8], len: usize) -> u8
 //              const [u8] x: input byte array
 //              size_t len:             Amount of bytes to be copied
 //              [u8]  b:        Condition bit; has to be in {0,1}
-pub fn cmov(r: &mut[u8], x: &[u8], len: usize, mut b: u8)
+pub fn cmov(r: &mut[u8], x: &[u8], len: usize, b: Choice)
 {
-  b = b.wrapping_neg();
   for i in 0..len {
-    r[i] ^= b & (x[i] ^ r[i]);
+    r[i].conditional_assign(&x[i], b);
   }
 }
